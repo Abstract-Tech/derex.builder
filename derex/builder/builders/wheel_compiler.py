9b8685b6be2a73227e01aafcfe59cfcdd897bc74
Silvio Tomatis <silviot@gmail.com>
2019-05-15 23:25:03 +0200

    Wheels cache

--- derex/builder/builders/wheel_compiler.py
+++ derex/builder/builders/wheel_compiler.py
@@ -3,14 +3,30 @@ from derex.builder import logger
 from derex.builder.builders.base import BaseBuilder
 from derex.builder.builders.base import create_builder
 from derex.builder.builders.base import load_conf
+from pathlib import Path
 from tempfile import TemporaryDirectory
+from typing import List
 
 import os
 
 
+WHEELS_CACHE = os.environ.get("WHEELS_CACHE")
+WC_VOLUMES: List[str] = []
+if WHEELS_CACHE is not None:
+    if Path(WHEELS_CACHE).is_dir():
+        WC_VOLUMES = ["-v", f"{WHEELS_CACHE}:/wheels_cache"]
+    else:
+        logger.error(
+            f'The directory "{WHEELS_CACHE} specified in WHEELS_CACHE does not exist"'
+        )
+
+
 class BuildahWheelCompiler(BaseBuilder):
-    """Uses a builder image to compile a set of python wheels,
-    and creates a new image by installing them in the base image.
+    """Use a builder image to compile a set of python wheels,
+    and create a new image by installing them in the base image.
+
+    Use the path in the environment variable WHEELS_CACHE as a wheel cache.
+    Copy the newly built wheels to the cache (maybe overwriting the already present ones).
     """
 
     json_schema = wheel_compiler_schema
@@ -28,7 +44,7 @@ class BuildahWheelCompiler(BaseBuilder):
         builder_container = self.buildah("from", builder_image)
         requirements_dir = "/etc/derex.builder.requirements"
         with TemporaryDirectory("wheelhouse") as tmp_whs:
-            volumes = ["-v", f"{tmp_whs}:/wheelhouse"]
+            volumes = ["-v", f"{tmp_whs}:/wheelhouse"] + WC_VOLUMES
             base_run = lambda *args: self.buildah_run(
                 container=base_container, args=list(args), extra_args=volumes
             )
@@ -45,13 +61,23 @@ class BuildahWheelCompiler(BaseBuilder):
                 logger.debug(open(src).read())
                 # If numpy is not installed scipy will refuse to compile.
                 # There is some build time potentially wasted. Maybe make it optional.
-                logger.info(builder_run(*"pip install -r".split(" "), f"{dest}"))
+                logger.info(builder_run(*"pip install -r".split(" "), dest))
                 logger.info(f"Compiling wheels for {requirement}")
+                wheel_cache_opts = (
+                    "" if WHEELS_CACHE is None else "--find-links /wheels_cache"
+                )
                 logger.info(
                     builder_run(
-                        *"pip wheel --wheel-dir=/wheelhouse -r".split(" "), f"{dest}"
+                        *f"pip wheel {wheel_cache_opts} --wheel-dir=/wheelhouse -r".split(
+                            " "
+                        ),
+                        dest,
                     )
                 )
+                if WHEELS_CACHE is not None:
+                    logger.info(
+                        builder_run("sh", "-c", "cp -rv /wheelhouse/* /wheels_cache/")
+                    )
             logger.info(f"Created wheeels:\n{'n'.join(os.listdir(tmp_whs))}")
             logger.info(base_run("sh", "-c", "pip install /wheelhouse/*"))
         self.buildah("commit", "--rm", base_container, self.dest)
